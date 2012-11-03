681cfe20bb3505f999fd19384da33e80e992abfc
maydogan <meltem.aydogan@bil.omu.edu.tr>
2012-11-03 18:31:39 +0200

    ..

--- php/find.php
+++ php/find.php
@@ -2,7 +2,7 @@
 if (!mysql_connect("localhost", "root", "12345"))
 	die("Error Username or Password");
 if (!mysql_select_db("foo"))
-	die("Error Database");
+	die("Error Database...");
 
 $result= mysql_query("select * from kul where name = '" . $_POST["ad"] . "'");
 $row = mysql_fetch_assoc($result);
